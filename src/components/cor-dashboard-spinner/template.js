5aed46022c784a2030d05c242f90ee8fa9b6bd66
Jesse Govart <govartjessy@gmail.com>
2019-05-28 15:38:40 +0200

    overview grid alignment

--- src/components/cor-dashboard-spinner/template.js
+++ src/components/cor-dashboard-spinner/template.js
@@ -7,22 +7,7 @@ export default {
 
     html(stroke, radius, circumference, normalizedRadius) {
         return `
-            <div class="spinner">
-                <div class="sk-fading-circle">
-                    <div class="sk-circle1 sk-circle"></div>
-                    <div class="sk-circle2 sk-circle"></div>
-                    <div class="sk-circle3 sk-circle"></div>
-                    <div class="sk-circle4 sk-circle"></div>
-                    <div class="sk-circle5 sk-circle"></div>
-                    <div class="sk-circle6 sk-circle"></div>
-                    <div class="sk-circle7 sk-circle"></div>
-                    <div class="sk-circle8 sk-circle"></div>
-                    <div class="sk-circle9 sk-circle"></div>
-                    <div class="sk-circle10 sk-circle"></div>
-                    <div class="sk-circle11 sk-circle"></div>
-                    <div class="sk-circle12 sk-circle"></div>
-                </div>
-            </div>
+            <div class="cor-dashboard-spinning-loader"></div>
         `
     }
 }
--- src/components/cor-dashboard/cor-dashboard.js
+++ src/components/cor-dashboard/cor-dashboard.js
@@ -2057,9 +2057,7 @@
                 data.sort( (a, b) => (a.total < b.total) ? 1 : -1 );
             }
 
-            console.log(this, this.offsetHeight);
-
-            var height = 300;
+            var height = 200;
 
             var yScale = d3.scaleLinear()
                 .domain([0, d3.max(data, d => {
@@ -2653,22 +2651,7 @@
 
         html(stroke, radius, circumference, normalizedRadius) {
             return `
-            <div class="spinner">
-                <div class="sk-fading-circle">
-                    <div class="sk-circle1 sk-circle"></div>
-                    <div class="sk-circle2 sk-circle"></div>
-                    <div class="sk-circle3 sk-circle"></div>
-                    <div class="sk-circle4 sk-circle"></div>
-                    <div class="sk-circle5 sk-circle"></div>
-                    <div class="sk-circle6 sk-circle"></div>
-                    <div class="sk-circle7 sk-circle"></div>
-                    <div class="sk-circle8 sk-circle"></div>
-                    <div class="sk-circle9 sk-circle"></div>
-                    <div class="sk-circle10 sk-circle"></div>
-                    <div class="sk-circle11 sk-circle"></div>
-                    <div class="sk-circle12 sk-circle"></div>
-                </div>
-            </div>
+            <div class="cor-dashboard-spinning-loader"></div>
         `
         }
     };
